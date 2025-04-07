83ee2e385d5c47984ad2bc496cdabab9235633c5
Diagne.js <cheikhounadiagne358@gmail.com>
2025-04-07 13:58:01 +0000

    Update useReactiveHtml.js

--- useReactiveHtml.js
+++ useReactiveHtml.js
@@ -1,1 +1,11 @@
-
+export const $useReactiveHtml = (s, value, d) => {
+  let text
+  block(() => {
+    text = document.createTextNode(value())
+    let tracker = document.querySelector(s)
+    tracker.parentElement.replaceChild(text, tracker)
+  })
+  effect(() => {
+    text.textContent = value()
+  }, d, false)
+}
